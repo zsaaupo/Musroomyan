0cb4e5706eb8f638a20e74c40bcbf52e5a0fc031
zsaaupo <zakariasarker407@gmail.com>
2022-05-18 16:53:39 +0600

    set 7 days time limit for JWT token

--- Customer/views.py
+++ Customer/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK, HTTP_203_NON_AUTHORITATIVE_INFORMATION, HTTP_226_IM_USED, HTTP_202_ACCEPTED, HTTP_406_NOT_ACCEPTABLE, HTTP_401_UNAUTHORIZED, HTTP_201_CREATED, HTTP_422_UNPROCESSABLE_ENTITY,
+from rest_framework.
 
 from .models import CustomerData
 
--- Mushroomyan/settings.py
+++ Mushroomyan/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,4 +132,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+# set time for JWT token
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
