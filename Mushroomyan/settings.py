81bfbab14b55f9688fa8dedce98e718cafbd86ed
zsaaupo <zakariasarker407@gmail.com>
2022-05-19 16:51:28 +0600

    linked with static and CSS

--- Mushroomyan/settings.py
+++ Mushroomyan/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 from datetime import timedelta
 
@@ -121,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
